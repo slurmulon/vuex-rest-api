11a44260015284ed480da94da17cf1210f4bd8c5
Christian Malek <grindchris@gmail.com>
2018-04-15 22:27:27 +0200

    startet writing tests

---
+++ tests/unit/Resource.test.js
@@ -0,0 +1,69 @@
+import axios from "axios"
+import Resource from "../../dist/Resource"
+import Axios from "axios";
+
+test("commit strings will be transformed", () => {
+    const commitStrings = new Map([
+        ["getPosts", "GET_POSTS"],
+        ["yetAnotherPost", "YET_ANOTHER_POST"],
+        ["cyber", "CYBER"]
+    ])
+
+    const getCommitString = Resource.prototype.getCommitString
+
+    commitStrings.forEach((should, is) => {
+        expect(getCommitString(is)).toBe(should)
+    })
+})
+
+test("dispatch strings will be transformed", () => {
+    const dispatchStrings = new Map([
+        ["getPosts", "getPosts"],
+        ["yetAnotherPost", "yetAnotherPost"],
+        ["cyber", "cyber"]
+    ])
+
+    const getDispatchString = Resource.prototype.getDispatchString
+
+    dispatchStrings.forEach((should, is) => {
+        expect(getDispatchString(is)).toBe(should)
+    })
+})
+
+describe("normalizedBaseURL", () => {
+    test("is empty string if no axios base url or Vapi base url is set", () => {
+        const resource = new Resource({})
+        expect(resource.normalizedBaseURL).toBe("")
+    })
+
+    test("is base url if Resource base url is set", () => {
+        const resource = new Resource({
+            baseURL: "https://resourceurl.com"
+        })
+        expect(resource.normalizedBaseURL).toBe("https://resourceurl.com")
+    })
+
+    test("is Resource base url (higher precedence than axios base url)", () => {
+        const axiosInstance = axios.create({
+            baseURL: "https://axiosurl.com"
+        })
+
+        const resource = new Resource({
+            baseURL: "https://resourceurl.com"
+        })
+
+        expect(resource.normalizedBaseURL).toBe("https://resourceurl.com")
+    })
+
+    test("is axios base url if Resource base url is not set but axios base url", () => {
+        const axiosInstance = axios.create({
+            baseURL: "https://axiosurl.com"
+        })
+
+        const resource = new Resource({
+            axios: axiosInstance
+        })
+
+        expect(resource.normalizedBaseURL).toBe("https://axiosurl.com")
+    })
+})
---
+++ tests/unit/tests.js
@@ -0,0 +1,40 @@
+/*
+# WHAT TO TEST
+
+- check if params and data will be available
+
+- check if state changes successfully
+  - on error
+  - on success
+
+- check if store will be initialized properly
+
+- check if state.pending.<property> will be set
+
+- check if state.error.<property> will be set
+
+- check if shorthand syntax methods work
+
+- check if convenient add method works
+
+- check if path and path fn work
+
+- check if onSuccess fn works
+
+- check if onError fn works
+
+- check if requestConfig works
+
+- check if createStateFn works
+
+- check getStore fn
+
+- check if calling the actions works without setting params/data if not needed
+
+*/
+
+import Vapi from "../../dist"
+
+test("", () => {
+
+})
